cd4f93ffd17f6855280d7c854f11338785dfeb4a
vl222cu <vl222cu@student.lnu.se>
2014-02-02 15:54:51 +0100

    Hoppas jag fått med allt i secretnumberklassen nu

--- 1dv6_1_4_gissa_det_hemliga_talet/1dv6_1_4_gissa_det_hemliga_talet/Model/SecretNumber.cs
+++ 1dv6_1_4_gissa_det_hemliga_talet/1dv6_1_4_gissa_det_hemliga_talet/Model/SecretNumber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 
@@ -14,10 +15,97 @@ namespace _1dv6_1_4_gissa_det_hemliga_talet.Model
 		private List<int> _previousGuesses;
 		public const int MaxNumberOfGuesses = 7;
 
+		// Ger ett värde som indikerar om en gissning kan göras eller inte
 		public bool CanMakeGuess
-		{ }
+		{
+			get
+			{
+				return !(PreviousGuesses.Count == MaxNumberOfGuesses 
+					|| Outcome.PreviousGuess == Outcome.NoMoreGuesses);
+			}
+		}
 
+		// Ger antalet gjorda gissningar 
 		public int Count
-		{ }
+		{
+			get { return PreviousGuesses.Count; }
+		}
+
+		// Sätter det hemliga talet
+		public int? Number
+		{
+			get 
+			{
+				if (CanMakeGuess)
+				{
+					return null;
+				}
+				return _number; 
+			}
+		}
+
+		// Ger resultat av den senast utförda gissningen
+		public Outcome Outcome { get; private set; }
+
+		// Ger referens till en samling innehållande gjorda gissningar
+		public ReadOnlyCollection<int> PreviousGuesses
+		{
+			get { return _previousGuesses.AsReadOnly(); }
+		}
+
+		// Initierar klassens medlemmar
+		public void Initialize()
+		{
+			_number = 0;
+
+			_previousGuesses.Clear();
+			Outcome = Outcome.Indefinite;
+			Random randomNumber = new Random();
+			_number = randomNumber.Next(1, 101);
+		}
+
+		// Returnerar värde på det gissade talet
+		public Outcome MakeGuess(int guess)
+		{
+			if (guess < 1 || guess > 100) 
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+
+			if (Count > MaxNumberOfGuesses)
+			{
+				throw new ApplicationException();
+			}
+
+			if (Count == MaxNumberOfGuesses)
+			{
+				return Outcome.NoMoreGuesses;
+			}
+
+			if (PreviousGuesses.Contains(guess))
+			{
+				return Outcome.PreviousGuess;
+			}
+
+			if (guess > _number)
+			{
+				return Outcome.High;
+			}
+			else if (guess < _number)
+			{
+				return Outcome.Low;
+			}
+			else
+			{
+				return Outcome.Correct;
+			}
+		}
+
+		//Konstruktor
+		public SecretNumber()
+		{
+			_previousGuesses = new List<int>(MaxNumberOfGuesses);
+			Initialize();
+		}
 	}
 }
